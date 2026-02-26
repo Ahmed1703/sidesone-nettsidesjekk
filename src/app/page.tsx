e1116887c14f18746c126dd560780862aeeeb05c
Ahmed <ahmedalsaatie@gmail.com>
2026-02-26 16:03:53 +0100

    Add homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Sidesone Nettsidesjekk</h1>
+      <p>Deployment successful.</p>
+    </main>
+  );
+}
